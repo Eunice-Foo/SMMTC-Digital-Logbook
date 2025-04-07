909f131f270a082847d1a94b7cced39b550ab027
Eunice_Foo <eunice_foo_yi@smmtc.uum.edu.my>
2025-04-07 10:09:23 +0800

    Control panel style done

--- components/export_preview_control.php
+++ components/export_preview_control.php
@@ -3,29 +3,39 @@
     <div class="profile-toggle">
         <label class="profile-checkbox">
             <input type="checkbox" id="includeProfile" checked onchange="toggleProfilePage()">
-            Include Profile Page
+            Include Profile
         </label>
     </div>
-    <div class="font-controls">
-        <div class="control-group">
-            <span class="font-label">Text:</span>
-            <button onclick="adjustFontSize('decrease')" class="font-button">A-</button>
-            <span id="currentFontSize">11pt</span>
-            <button onclick="adjustFontSize('increase')" class="font-button">A+</button>
+    
+    <div class="controls-container">
+        <div class="control-section">
+            <span class="control-label">Text Size</span>
+            <div class="control-group">
+                <button onclick="adjustFontSize('decrease')" class="font-button">−</button>
+                <span id="currentFontSize">11pt</span>
+                <button onclick="adjustFontSize('increase')" class="font-button">+</button>
+            </div>
         </div>
-        <div class="control-group">
-            <span class="font-label">Heading:</span>
-            <button onclick="adjustHeadingSize('decrease')" class="font-button">A-</button>
-            <span id="currentHeadingSize">12pt</span>
-            <button onclick="adjustHeadingSize('increase')" class="font-button">A+</button>
+        
+        <div class="control-section">
+            <span class="control-label">Heading Size</span>
+            <div class="control-group">
+                <button onclick="adjustHeadingSize('decrease')" class="font-button">−</button>
+                <span id="currentHeadingSize">14pt</span>
+                <button onclick="adjustHeadingSize('increase')" class="font-button">+</button>
+            </div>
         </div>
-        <div class="control-group">
-            <span class="font-label">Media:</span>
-            <button onclick="adjustMediaSize('decrease')" class="font-button">-</button>
-            <span id="currentMediaSize">150px</span>
-            <button onclick="adjustMediaSize('increase')" class="font-button">+</button>
+        
+        <div class="control-section">
+            <span class="control-label">Media Size</span>
+            <div class="control-group">
+                <button onclick="adjustMediaSize('decrease')" class="font-button">−</button>
+                <span id="currentMediaSize">150px</span>
+                <button onclick="adjustMediaSize('increase')" class="font-button">+</button>
+            </div>
         </div>
     </div>
-    <button onclick="window.print()" class="print-button">Print Document</button>
+    
+    <button onclick="window.print()" class="print-button">Print</button>
 </div>
 </body>
